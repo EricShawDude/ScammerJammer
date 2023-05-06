adab251e69c445ea5f6c559d7af4d81785f3269b
KlassicKanadian <103152351+KlassicKanadian@users.noreply.github.com>
2023-05-06 17:45:50 -0400

    added scripts for html

--- Firefail/public/scripts.js
+++ Firefail/public/scripts.js
@@ -1,9 +1,13 @@
 function pull() {
-    var input = document.getElementById("search-field").value;
-    console.log("Search input: " + input);
+  var input = document.getElementById("searchField").value;
+  document.getElementById("title").innerHTML = input;
+  changePage("index.html");
   }
 
 function search(){
 
 }
 
+function changePage(url) {
+  window.location.href = url;
+}
