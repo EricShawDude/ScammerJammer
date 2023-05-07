710950e5801c4ed11e4db24dca1c57e4c0fa58ce
alf0am <20ads15@queensu.ca>
2023-05-07 05:21:15 -0400

    ScammerFailer
    We won Mister white. We did it

--- Firefail2/public/app.js
+++ Firefail2/public/app.js
@@ -1,14 +1,11 @@
- const db = firebase.firestore();
+db = firebase.firestore();
+var username = "unknown";
+//GOOGSHIT STUFF
 
 const auth = firebase.auth();
-
-const whenSignedIn = document.getElementById('whenSignedIn');
-const whenSignedOut = document.getElementById('whenSignedOut');
-
 const signInBtn = document.getElementById('signInBtn');
 const signOutBtn = document.getElementById('signOutBtn');
 
-const userDetails = document.getElementById('userDetails');
 
 const provider = new firebase.auth.GoogleAuthProvider();
 signInBtn.onclick = () => auth.signInWithPopup(provider)
@@ -16,19 +13,23 @@ signOutBtn.onclick = () => auth.signOut()
 
 auth.onAuthStateChanged(user => {
     if (user){
-        whenSignedIn.hidden = false;
-        whenSignedOut.hidden = true;
-        userDetails.innerHTML = `Well done dumbass ${user.displayName}`
+        signInBtn.hidden = true;
+        signOutBtn.hidden = false;
+        username = user.displayName;
     }else{
-        whenSignedIn.hidden = true;
-        whenSignedOut.hidden = false;
-        userDetails.innerHTML = 'GERALD!!!'
+        signInBtn.hidden = false;
+        signOutBtn.hidden = true;
+        username = "unknown";
     }
 });
 
+
+//PAST FORM SHIT
+/*
 const phonelist = document.querySelector("#phoneform");
 const searchlist = document.querySelector("#searchform");
 const commentlist = document.querySelector("#commentform");
+//phone number submit button listener
 phonelist.addEventListener('submit', (e) => {
     e.preventDefault();
     db.collection('dumbshit').add({
@@ -36,16 +37,157 @@ phonelist.addEventListener('submit', (e) => {
     })
     addDocLinear(phonelist.name.value, commentlist.name.value);
 })
+//search button listener
 searchlist.addEventListener('submit', (e) => {
     e.preventDefault();
     renderReportsFromNum(phonelist.name.value);
 })
 
+*/
 
-const numlist = document.querySelector('#numlist');
+/*
+function pull() {
+    var input = document.getElementById("searchField").value;
+    document.getElementById("title").innerHTML = input;
+    changePage("index.html");
+}
+*/ 
 
-//add stuff with a comment
-function addDocLinear(num, comment = 'failure!'){
+var pagetemp = 0
+var pagetempreports = 0
+
+
+function notscam(){
+    db.collection('failtemp').get().then((snapshot) =>{
+        document.getElementById("button_report").disabled = true;
+        snapshot.docs.forEach(doc => {
+            console.log("Frank won.");
+            pagetemp = doc.data().temp;
+            console.log(pagetemp);
+            setTimeout(() => { 
+                document.getElementById("searchField").value = pagetemp;
+                document.getElementById('phone_number').innerHTML = pagetemp;
+                
+                document.getElementById("button_report").disabled = false;
+                 }, 500);
+            
+        })      
+    })
+}
+function scam(){
+    document.getElementById("button_report").disabled = true;
+    
+    db.collection('failtemp').get().then((snapshot) =>{
+        snapshot.docs.forEach(doc => {
+            console.log("Frank told the man on the other end his credit card info. It's joever.");
+            pagetemp = doc.data().temp;
+            setPageTempReports();
+            console.log(pagetemp);
+            
+
+            setTimeout(() => { 
+            renderReportsFromNum(pagetemp);
+            document.getElementById("searchField").value = pagetemp;
+            document.getElementById('phone_number').innerHTML = pagetemp;
+            document.getElementById('reported').innerHTML = "Reported  "+ pagetempreports + " times";
+            document.getElementById("button_report").disabled = false;
+            
+             }, 500);
+            
+        })      
+    })
+
+}
+
+function setPageTempReports(){
+    db.collection('numbers').get().then((snapshot) =>{
+        snapshot.docs.forEach(doc => {
+            if (doc.data().number == pagetemp){
+                pagetempreports = doc.data().reports;
+            }      
+        })
+    })
+}
+function search(){
+
+    var number = document.getElementById("searchField").value;
+    console.log("Frank is")
+    console.log(number)
+    number = number.replace(/\D/g,'')
+    if(number == ""){
+        return;
+    }else{
+        document.getElementById("searchField").readOnly = true;
+        document.getElementById("button_search").disabled = true;
+        try {
+            document.getElementById("button_report").disabled = true;
+          } catch (error) {
+            console.error(error);
+          }
+        renderReportsFromNum(number);
+        //if number exists then scam
+        found = false;
+        db.collection('numbers').get().then((snapshot) =>{
+            snapshot.docs.forEach(doc => {
+                console.log("Frank waz here")
+                console.log(number)
+                console.log(doc.data().number)
+                console.log((doc.data().number == number));
+                if (doc.data().number == number){
+                    console.log("number found");
+                    found = true;
+                    addTemp(number)
+                    //😭
+                    setTimeout(() => { changePage("scam.html"); }, 1000);
+                    return;
+                }      
+            })
+            if (!found){     
+                addTemp(number)
+                //this is so bad vvvv what if their internet dies or smth 😭
+                setTimeout(() => { changePage("not_scam.html"); }, 1000);
+            }
+            
+        })
+
+        
+        
+    
+    }
+}
+function reportdebug(num, message = "😭"){
+    addDocLinear(num, message);
+    return;
+}
+function reportinitial(){
+    document.getElementById("button_report").innerHTML = "Reported 🗸";
+    document.getElementById("button_report").disabled = true;
+    
+    
+    console.log(pagetemp);
+    addDocLinear(pagetemp);
+}
+
+function reportalready(){
+    document.getElementById("button_report").innerHTML = "Reported 🗸";
+    document.getElementById("button_report").disabled = true;
+    addDocLinear(pagetemp);
+}
+
+
+function changePage(url) {
+    window.location.href = url;
+}
+function addTemp(num){
+    db.collection("failtemp").doc("temp").set({
+        temp: num
+      }).then(function() {
+        console.log("Frank is temporary.");
+      });
+}
+
+function addDocLinear(num, comment = "default"){
+    comment = username + ":" + comment;
     gaming = true;
     db.collection('numbers').get().then((snapshot) =>{
         snapshot.docs.forEach(doc => {
@@ -91,8 +233,10 @@ function addDocLinear(num, comment = 'failure!'){
 
 }
 
-function addDocNew(num, comment = 'failure fail'){
-    console.log("gaming is a go")
+
+
+function addDocNew(num, comment = 'default'){
+    console.log("gaming is a go");
     const uid = db.collection("tmp").doc().id
     db.collection("numbers").add({
         number: num,
@@ -110,28 +254,28 @@ function addDocNew(num, comment = 'failure fail'){
 
 
 function rendernumbers(doc){
-    let li = document.createElement('li');
-    let reports = document.createElement('span');
-    let num = document.createElement('span');
-    let commentlist = document.createElement('li');
-
-    li.setAttribute('data-id', doc.id);
-    reports.textContent = doc.data().reports.toString() + " reports ";
-    num.textContent = doc.data().number.toString();
-
+    
     const commentarray = doc.data().comments;
     const timearray = doc.data().times; 
-    st = "";
+    st = [];
     commentarray.forEach(function (item, index) {
-        st = st + index.toString() + " \"" + item + "\" at " + timearray[index].toDate().toString() + "\n"; 
+        st.push("#" + (index +1).toString() + " \"" + item + "\" at " + timearray[index].toDate().toString() + "\n");
     });
-    commentlist.textContent = st;
 
-    li.appendChild(reports);
-    li.appendChild(num);
-    li.appendChild(commentlist)
+    let ul = document.createElement('ul');
+    let li = document.createElement('li');
 
-    numlist.appendChild(li);
+    document.querySelector('#numlist').appendChild(ul);
+
+    st.forEach((item) => {
+        li.innerHTML += item;
+
+        // Add li to the ul
+        ul.appendChild(li);
+
+        // Reset the list item
+        li = document.createElement('li')
+    });
 }
 
 
